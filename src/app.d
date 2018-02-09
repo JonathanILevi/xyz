f2c2594ba4adb331fe3c38c54fc72b3535a380c1
Unknown <JonathanILevi@gmail.com>
2018-02-08 18:45:18 -0600

    Init Dub and added code.

---
+++ src/app.d
@@ -0,0 +1,9 @@
+pragma(inline, true) public T x(T)(T[] vec) {
+	return vec[0];
+}
+pragma(inline, true) public T y(T)(T[] vec) {
+	return vec[1];
+}
+pragma(inline, true) public T z(T)(T[] vec) {
+	return vec[2];
+}
